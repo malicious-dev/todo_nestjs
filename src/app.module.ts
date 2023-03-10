6588c3f49f815ab1e4483265a69d46310b9f3f59
malicious-dev <Programming1hars@gmail.com>
2023-03-10 22:47:29 +0530

    chnages

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.local.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host : configService.get("DATABASE_HOST"),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get("DATABASE_USERNAME"),
+        password: configService.get("DATABASE_PASSWORD"),
+        synchronize: configService.get<boolean>("DATABASE_SYNC"),
+        logging: configService.get<boolean>("DATABASE_LOGGING"),
+        entities: [__dirname+'/**/*entity[.ts, .js'],
+      })
+    }),
+    UserModule,
+    TodoModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
